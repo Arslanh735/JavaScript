e548d0638682be866a71a018ada512ab4fe8d8d2
Arslanh735 <arslanh735@gmail.com>
2026-01-30 01:07:19 +0500

    Test 20 complete

--- Javascript practice Assignment Set 5/app.js
+++ Javascript practice Assignment Set 5/app.js
@@ -20,3 +20,42 @@
 // console.log(lowerCase);
 
 
+// Task # 17
+
+// var str = "Arslan Haider";
+
+// console.log(str.length);
+
+
+// Task # 18
+
+// var randomNumber = Math.floor(Math.random() * 10) + 1;
+
+// console.log(randomNumber);
+
+
+
+// Task # 19
+
+// var num = +prompt("Enter Number");
+// // num = parseFloat(num);
+
+//     console.log("Round", Math.round(num))
+//     console.log("Floor", Math.floor(num))
+//     console.log("Ceil", Math.ceil(num))
+
+// Task # 20
+
+// var num1 = 200;
+
+// num1 = String(num1);
+
+// console.log(num1);
+// console.log(typeof(num1));
+
+// var num2 = "700";
+
+// num2 = Number(num2)
+
+// console.log(num2);
+// console.log(typeof(num2));
